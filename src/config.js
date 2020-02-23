6fd1e75efb054c34966fe4aa3d20192ce1711684
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-02-23 18:34:45 -0300

    Read the API's domain from .env files

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const Configs = Object.freeze({
-  MAX_LENGTH: 44,                          // maximum length for a generated pw
-  MIN_LENGTH: 4,                           // minimum length for a generated pw
-  DEFAULT_LENGTH: 32,                      // default length for a generated pw
-  SHAPASS_SERVICE: 'shapass',              // name of the service when generating pw for shapass
-  LOGIN_COOKIE_NAME: 'shapasslogin',       // name of the cookie to save the user token
-  API_URL: 'https://shapass.com/api',      // URL of the API
-  PASSWORD_VISIBILITY_TIMEOUT: 8000,       // time before passwords are hidden
-  DEFAULT_ALGORITHM: 'sha256-bin',         // sha256-str, sha256-bin, sha256-bin-alfanum
-  REMEMBER_ME_DAYS: 90,                    // keep the login cookie for this amount of days
+  MAX_LENGTH: 44,                               // maximum length for a generated pw
+  MIN_LENGTH: 4,                                // minimum length for a generated pw
+  DEFAULT_LENGTH: 32,                           // default length for a generated pw
+  SHAPASS_SERVICE: 'shapass',                   // name of the service when generating pw for shapass
+  LOGIN_COOKIE_NAME: 'shapasslogin',            // name of the cookie to save the user token
+  API_URL: `${process.env.VUE_APP_DOMAIN}/api`, // URL of the API
+  PASSWORD_VISIBILITY_TIMEOUT: 8000,            // time before passwords are hidden
+  DEFAULT_ALGORITHM: 'sha256-bin',              // sha256-str, sha256-bin, sha256-bin-alfanum
+  REMEMBER_ME_DAYS: 90,                         // keep the login cookie for this amount of days
 
   boundedOutputLength: function(l) {
     if (l < Configs.MIN_LENGTH) {
